5236bd1bbe865ad882e71ade72344b84691b02dd
iamhrk <iamhrk8@gmail.com>
2025-07-17 09:40:00 +0530

    Display tasks component on clicking user

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -9,6 +9,38 @@ import { TaskComponent } from "./task/task.component";
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) userId!: string;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn all the basic and advanced features of angular',
+      dueDate: '2025-12-31'
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Build Prototype',
+      summary: 'Build a prototype of the client website',
+      dueDate: '2025-07-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary: 'Prepare and describe an issue template for project management',
+      dueDate: '2025-05-31'
+    },
+    
+  ]
 
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
+
+  onCompleteTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
 }
